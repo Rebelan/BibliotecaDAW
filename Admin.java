803865fbe78630e129f8f8394a54044b7ea6ed62
HelMoonflare <alexanderplanton@gmail.com>
2025-01-21 17:13:48 +0100

    atributos admin

--- Admin.java
+++ Admin.java
@@ -1,4 +1,14 @@
 public class Admin {
     private String nombre;
     private String passwd;
+
+
+    public Admin(){
+        this.nombre = "";
+        this.passwd = "";
+    }
+
+    public Admin(String nombre, String passwd){
+        
+    }
 }
