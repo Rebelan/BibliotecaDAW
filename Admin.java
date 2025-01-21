7d4109088c873d596a2580a49824ba85f51049b6
HelMoonflare <alexanderplanton@gmail.com>
2025-01-21 16:58:04 +0100

    admin y libros

---
+++ Admin.java
@@ -0,0 +1,4 @@
+public class Admin {
+    private String nombre;
+    private String passwd;
+}
---
+++ Libros.java
@@ -0,0 +1,6 @@
+public class Libros {
+    private String titulo;
+    private String autor;
+    private String categoria;
+    private boolean disponible;
+}
