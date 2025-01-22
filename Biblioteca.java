aaecbe4cb910251dfeee67fbe5c5da1073396b67
Rebelan <abelcm2013@gmail.com>
2025-01-22 17:05:26 +0100

    Biblio es el main

--- Biblioteca.java
+++ Biblioteca.java
@@ -1,17 +1,7 @@
 public class Biblioteca {
-    private String nomBiblio;
-    private Libro[] libros;
-    private Usuario[] usuarios;
 
-    public Biblioteca() {
-        this.nomBiblio = "Biblioteca";
-        this.libros = new Libro[10];
-        this.usuarios = new Usuario[10];
-    }
-
-    public Biblioteca(String nomBiblio) {
-        this();
-        this.nomBiblio = nomBiblio;
+    public static void main(String[] args) {
+        
     }
 
 }
