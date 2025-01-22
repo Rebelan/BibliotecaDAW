878c02ab34deebc915fd5b1b6995228ef59c8436
Rebelan <abelcm2013@gmail.com>
2025-01-22 16:36:22 +0100

    Constructores biblitoteca

--- Biblioteca.java
+++ Biblioteca.java
@@ -2,4 +2,16 @@ public class Biblioteca {
     private String nomBiblio;
     private Libro[] libros;
     private Usuario[] usuarios;
+
+    public Biblioteca() {
+        this.nomBiblio = "Biblioteca";
+        this.libros = new Libro[10];
+        this.usuarios = new Usuario[10];
+    }
+
+    public Biblioteca(String nomBiblio) {
+        this();
+        this.nomBiblio = nomBiblio;
+    }
+
 }
