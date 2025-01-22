5c9ed4b90c8b69c8815bb042700ef3a2f7462ac8
Rebelan <abelcm2013@gmail.com>
2025-01-22 17:52:07 +0100

    Parte del main y actualizacion de funciones de añadir usuario

--- Biblioteca.java
+++ Biblioteca.java
@@ -1,11 +1,57 @@
+import java.util.Scanner;
+
 public class Biblioteca {
+
+    public static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
         Usuario usuario1 = new Usuario("Pepe", "1234", true);
         GestorUsuarios gestor = new GestorUsuarios();
-        gestor.nuevoUsuarioAdmin(usuario1);
-        Usuario usuario2 = new Usuario("Juan", "5678", false);
-        gestor.nuevoUsuarioNormal(usuario2);
+        gestor.nuevoUsuario(usuario1);
+        Usuario usuario2 = new Usuario("Juan", "5678", true);
+        gestor.nuevoUsuario(usuario2);
+
+        boolean flag = true;
+        while (flag) {
+            System.out.println("Introduce una opción");
+            menuVisual();
+            int op = Integer.parseInt(sc.nextLine());
+            switch (op) {
+                case 3:
+                    System.out.println("Introduce el nombre del usuario: ");
+                    String nombre = sc.nextLine();
+                    System.out.println("Introduce la contraseña: ");
+                    String passwd = sc.nextLine();
+                    System.out.println("¿Es administrador? (s/n)");
+                    String admin = sc.nextLine();
+                    boolean esAdmin = false;
+                    if (admin.equals("s")) {
+                        esAdmin = true;
+                    }
+                    Usuario usuario = new Usuario(nombre, passwd, esAdmin);
+                    gestor.nuevoUsuario(usuario);
+                    break;
+                case 7:
+                    gestor.ConsultarInformacionUsuarios();
+                    break;
+                case 9:
+                    flag = false;
+                    break;
+            }
+        }
+
     }
 
+    public static void menuVisual() {
+        System.out.println("1. Añadir un libro");
+        System.out.println("2. Eliminar un libro");
+        System.out.println("3. Añadir un usuario");
+        System.out.println("4. Eliminar un usuario");
+        System.out.println("5. Prestar un libro");
+        System.out.println("6. Devolver un libro");
+        System.out.println("7. Consultar información de los usuarios");
+        System.out.println("8. Consultar información de los libros");
+        System.out.println("9. Salir");
+
+    }
 }
--- GestorUsuarios.java
+++ GestorUsuarios.java
@@ -1,46 +1,26 @@
-import java.util.Scanner;
 
 public class GestorUsuarios {
     private Usuario[] usuarios;
     private int numUsuarios;
-    
-
-    public static Scanner sc = new Scanner(System.in);
 
     public GestorUsuarios() {
         this.usuarios = new Usuario[20];
         this.numUsuarios = 0;
     }
 
-    public void nuevoUsuarioAdmin(Usuario usuario) {
-        if (usuario.getAdmin() == true) {
-            System.out.println("Introduce el nombre del administrador");
-            String nombre = sc.nextLine();
-            System.out.println("Introduce la contraseña del administrador");
-            String passwd = sc.nextLine();
-            Usuario usuarioN = new Usuario(nombre,passwd,true);
-            usuarios[numUsuarios] = usuarioN;
+    public void nuevoUsuario(Usuario usuario) {
+        usuarios[numUsuarios] = usuario;
         numUsuarios++;
-        System.out.println("Usuario añadido correctamente");
-        }else{
-            System.out.println("No tienes permisos para añadir un usuario");
-        }
-        
     }
-    public void nuevoUsuarioNormal(Usuario usuario) {
-        if (usuario.getAdmin() == true) {
-            System.out.println("Introduce el nombre del usuario");
-            String nombre = sc.nextLine();
-            System.out.println("Introduce la contraseña del usuario");
-            String passwd = sc.nextLine();
-            Usuario usuarioNormal = new Usuario(nombre,passwd,false);
-            usuarios[numUsuarios] = usuarioNormal;
-        numUsuarios++;
-        }else{
-            System.out.println("No tienes permisos para añadir un usuario");
+
+    public void ConsultarInformacionUsuarios() {
+        System.out.println("Los usuarios de la biblioteca son: ");
+        for (int i = 0; i < numUsuarios; i++) {
+            System.out.println("Nombre: " + usuarios[i].getNomUsuario());
+            System.out.println("Contraseña: " + usuarios[i].getPasswd());
+            System.out.println("Es administrador: " + usuarios[i].getAdmin());
+            System.out.println("-------------");
         }
-        
     }
-    
 
 }
