a4a5590d6cfbcd900e467ea87957f7e64d14fc1a
HelMoonflare <alexanderplanton@gmail.com>
2025-01-22 17:41:31 +0100

    buscar y eliminar libros

--- Libros.java
+++ Libros.java
@@ -1,5 +1,7 @@
 // Esta clase contiene un array con todos los libros de la biblioteca
 
+import java.util.Arrays;
+
 public class Libros {
    private Libro[] libros;
    private int numLibros;
@@ -11,7 +13,66 @@ public class Libros {
    }
 
    public void nuevoLibro(Libro libro) {
-    libros[numLibros] = libro;
-    numLibros++;
+      libros[numLibros] = libro;
+      numLibros++;
    }
+
+   private int buscarIdLibro(String titulo) {
+      int buscar = -1;
+      for (int i = 0; i < numLibros && buscar == -1; i++) {
+         if (libros[i].getTitulo().equals(titulo)) {
+            buscar = i;
+         }
+      }
+      return buscar;
+   }
+
+   public Libro buscarLibro(String titulo) {
+      Libro buscar = null;
+      int indice = buscarIdLibro(titulo);
+      if (indice != -1) {
+         buscar = libros[indice];
+      }
+      return buscar;
+
+   }
+
+   public Libro[] buscarPorAutor(String autor) {
+      Libro[] resultado = new Libro[tam];
+      int autorLleno = 0;
+      for (int i = 0; i < numLibros; i++) {
+         if (libros[i].getAutor().equals(autor)) {
+            resultado[autorLleno] = libros[i];
+            autorLleno++;
+         }
+      }
+      return Arrays.copyOf(resultado, autorLleno);
+   }
+
+   public Libro[] buscarPorCategoria(String categoria){
+      Libro[] resultado = new Libro[tam];
+      int catLlena = 0;
+      for (int i = 0; i < numLibros; i++) {
+         if(libros[i].getCategoria().equals(categoria)){
+            resultado[catLlena] = libros[i];
+            catLlena++;
+         }
+      }
+      return Arrays.copyOf(resultado, catLlena);
+   }
+
+   public boolean eliminarLibro(String titulo) {
+      int indice = buscarIdLibro(titulo);
+      if (indice != -1) {
+         for (int i = indice; i < numLibros - 1; i++) {
+            libros[i] = libros[i + 1];
+         }
+         libros[--numLibros] = null;
+         return true;
+      } else {
+         return false;
+      }
+   }
+
+   
 }
